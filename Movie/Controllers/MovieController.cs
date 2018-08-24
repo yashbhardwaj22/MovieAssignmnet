b403c2c5b9c68375659c0dcdb46cfb5e5a7da948
yash bhardwaj <yashbhardwaj95@gmail.com>
2018-08-24 15:13:15 +0530

    first commit

---
+++ Movie/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
+
+namespace Movie.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            {
+                ArrayList moviedata = getData();
+
+
+                return View(moviedata);
+            }
+            ArrayList getData()
+            {
+                Moviedata data = new Moviedata();
+                return data.getData();
+            }
+        }
+    }
+}
